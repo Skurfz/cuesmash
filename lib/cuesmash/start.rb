85f03d12e52fddb46f82432ac45a3aa27f63b53b
Jarod McBride <jarod@ustwo.com>
2014-09-17 13:48:33 +0100

    setting up the start class

--- lib/cuesmash/start.rb
+++ lib/cuesmash/start.rb
@@ -1,24 +1,30 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+require 'thor'
+require 'cuesmash'
+
 module Cuesmash
 
   class Start < Thor
 
     desc 'init', 'set up the project'
     def init
+      Cuesmash::Setup.setup
       # Do stuff to set up the directory
     end
 
-    desc 'run OPTIONS', 'Usage: cuesmash [OPTIONS]
-                        --tags -t the tags to pass to cucumber, for multiple tags pass one per tag
-                        --scheme -s the Xcode scheme to build
-                        --ios -i the iOS version to build with
-                        --output -o The output directory for the test report
-                        --format -f The format of the test report
-                        --server -s'
-    def run(*options)
-      Command.execute(*options)
+    desc 'test OPTIONS', "Usage: cuesmash [OPTIONS]"
+    long_desc <<-LONGDESC
+      --tags -t the tags to pass to cucumber, for multiple tags pass one per tag\n
+      --scheme -s the Xcode scheme to build\n
+      --ios -i the iOS version to build with\n
+      --output -o The output directory for the test report\n
+      --format -f The format of the test report\n
+      --server -s
+    LONGDESC
+    def test(*options)
+      Cuesmash::Command.execute(*options)
     end
   end
 end
